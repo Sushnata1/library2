78aacfb9a8c715b50933c5642013276997e92ac3
Sushnata1 <coolsushnata@gmail.com>
2023-12-03 20:31:59 +0530

    added mock and pas datasource

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,7 +1,14 @@
+import config from "./dataSources/dataSource.config.js";
+
+import mockDataSource from "./dataSources/mockDataSource.js";
+import pasDataSource from "./dataSources/pasDataSource.js";
+
+var obj = (config.greet == "mock") ? new mockDataSource() : new pasDataSource();
+
 export default {
     Query:{
         greet:()=>{
-            return "Hello world"
+            return obj.greet();
         }
     }
 }
